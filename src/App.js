6e2819f56dcc12bb13205a91b2c8b2150dbe44b2
Fidelya1988 <oleksandram1988@gmail.com>
2021-07-09 13:37:43 +0300

    new version

--- src/App.js
+++ src/App.js
@@ -18,11 +18,20 @@ import {
   takeUntil,
   switchMap,
   delayWhen,
+  mapTo,
+ mergeWith,
+ startWith
 } from "rxjs/operators";
 
 const secondsObserv = interval(1000);
 const minutesObserv = interval(50 * 60);
 const hoursObserv = interval(50 * 60 * 60);
+const getElem = (id) => document.getElementById(id);
+
+const fromClick = (id) => fromEvent(getElem(id), "click");
+const fromClickAndMapTo = (id, obj) => fromClick(id).pipe(mapTo(obj));
+
+const setValue = (val) => (getElem("counter").innerText = val.toString());
 
 const getTimeUnits = (timeUnits) => {
   return timeUnits.pipe(
@@ -34,56 +43,57 @@ const getTimeUnits = (timeUnits) => {
 
 const useObservable = (observable, setter, setIsRunning, isRunning) => {
   useEffect(() => {
-    // const wait$ = fromEvent(document.getElementById("wait"), "click");
-
-    // const subscription = observable.subscribe((result) => {
-    //   setter(result);
-    //   if (!isRunning) {
-    //     subscription.unsubscribe();
-    //   }
-    // });
-
-    
-    const start$ = fromEvent(document.getElementById("start"), "click").pipe(
-      switchMap((e) => observable)
-    )
-    const sub2 = start$.subscribe((result) => {
-      setter(result);
-      setIsRunning(true);
-   
-        // if (!isRunning) {
-        //   sub2.unsubscribe();
-        // }
-      
-    })
-    const reset$ = fromEvent(document.getElementById("reset"), "click").pipe(
-      switchMap((e) => observable)
+    const events$ = mergeWith(
+      fromClickAndMapTo("start", { count: true }),
+      fromClickAndMapTo("wait", { count: false }),
+      fromClickAndMapTo("reset", { value: 0 })
     );
-    const subscription = reset$.subscribe((result) => {
-      setter(result);
-      setIsRunning(true);
-      sub2.complete()
-    });
-    return () => {subscription.unsubscribe()
-    sub2.unsubscribe()};
+
+    // const timer$ = events$().pipe(startWith({ count: false, speed: 1000, value: 0, countup: true, increase: 1 }))
+   
   }, []);
 };
+// const start$ = fromEvent(document.getElementById("start"), "click").pipe(
+//   switchMap((e) => observable)
+// )
+// const sub2 = start$.subscribe((result) => {
+//   setter(result);
+//   setIsRunning(true);
+
+// if (!isRunning) {
+//   sub2.unsubscribe();
+// }
+
+// })
+// const reset$ = fromEvent(document.getElementById("reset"), "click").pipe(
+//   switchMap((e) => observable)
+// );
+// const subscription = reset$.subscribe((result) => {
+//   setter(result);
+//   setIsRunning(true);
+//   sub2.complete()
+// });
+// return () => {subscription.unsubscribe()
+// sub2.unsubscribe()};
+//
+//   []);
+// };
 
 const App = () => {
   const [second, setseconds] = useState(0);
-  const [minute, setMinutes] = useState(0);
-  const [hour, setHours] = useState(0);
+  // const [minute, setMinutes] = useState(0);
+  // const [hour, setHours] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [isWaiting, setIsWaiting] = useState(false);
 
   useObservable(getTimeUnits(secondsObserv), setseconds, setIsRunning);
-  useObservable(getTimeUnits(minutesObserv), setMinutes, setIsRunning);
-  useObservable(getTimeUnits(hoursObserv), setHours, setIsRunning);
+  // useObservable(getTimeUnits(minutesObserv), setMinutes, setIsRunning);
+  // useObservable(getTimeUnits(hoursObserv), setHours, setIsRunning);
 
   return (
     <div className="App">
-      <span>{hour < 10 ? `0${hour}` : hour} : </span>
-      <span>{minute < 10 ? `0${minute}` : minute} : </span>
+      {/* <span>{hour < 10 ? `0${hour}` : hour} : </span>
+      <span>{minute < 10 ? `0${minute}` : minute} : </span> */}
       <span>{second < 10 ? "0" + second : second} </span>
       <div>
         <button
